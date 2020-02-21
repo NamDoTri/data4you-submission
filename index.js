d37ed1a5ce4acd4cdc55000296fd10b707fc433e
Tri Nam Do <t8dotr00@students.oamk.fi>
2020-02-21 14:32:12 +0200

    Drive upload works

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const generateScreenshot = require('./generateScreenshot');
+const generateScreenshot = require('./src/js/generateScreenshot');
 const screenshotmachine = require('screenshotmachine');
+const uploadToDrive = require('./src/js/uploadDrive');
 
 // parse arguments from command prompt
 let apiKey = process.argv[2]; // for security reasons, API key will be provided at run time
@@ -13,12 +14,14 @@ let imgUrl = generateScreenshot(apiKey, givenUrl);
 // create Downloads folder 
 if(!fs.existsSync('./Downloads')) fs.mkdirSync('./Downloads');
 
-// download the screenshot to local storage
+// // download the screenshot to local storage
+// TODO: take advantage of stream
 let path = `./Downloads/${fileName}.jpg`;
 let ws = fs.createWriteStream(path);
+
 ws.on('close', () => {
     console.log('1 screenshot saved to ' + path);
-})
-screenshotmachine.readScreenshot(imgUrl).pipe(ws);
+    uploadToDrive(`${fileName}.jpg`, path)
+});
 
-// upload to Google Drive
+screenshotmachine.readScreenshot(imgUrl).pipe(ws);
